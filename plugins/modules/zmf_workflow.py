8bc6beb00bf81e60c5f52674a31fc26c1930fee1
EricLiuAtIbm <xmliuxm@cn.ibm.com>
2021-12-23 11:59:50 +0800

    fix documentation issues

--- plugins/modules/zmf_workflow.py
+++ plugins/modules/zmf_workflow.py
@@ -366,10 +366,18 @@ notes:
 """
 
 EXAMPLES = r"""
+- name: Authenticate with z/OSMF server by username/password, and register the result for later use.
+  zmf_authenticate:
+    zmf_host: "{{ zmf_host }}"
+    zmf_port: "{{ zmf_port }}"
+    zmf_user: "{{ zmf_user }}"
+    zmf_password: "{{ zmf_password }}"
+  register: result_auth
+
 - name: Compare whether a workflow with the given name already exists
   ibm.ibm_zosmf.zmf_workflow:
     state: "existed"
-    zmf_host: "sample.ibm.com"
+    zmf_credential: "{{ result_auth }}"
     workflow_name: "ansible_sample_workflow_SY1"
     workflow_file: "/zosmf/workflow_def/workflow_sample_automation_steps.xml"
     workflow_host: "SY1"
@@ -377,7 +385,7 @@ EXAMPLES = r"""
 - name: Create a workflow if it does not exist, and start it
   ibm.ibm_zosmf.zmf_workflow:
     state: "started"
-    zmf_host: "sample.ibm.com"
+    zmf_credential: "{{ result_auth }}"
     workflow_name: "ansible_sample_workflow_{{ inventory_hostname }}"
     workflow_file: "/zosmf/workflow_def/workflow_sample_automation_steps.xml"
     workflow_host: "{{ inventory_hostname }}"
@@ -385,13 +393,13 @@ EXAMPLES = r"""
 - name: Delete a workflow if it exists
   ibm.ibm_zosmf.zmf_workflow:
     state: "deleted"
-    zmf_host: "sample.ibm.com"
+    zmf_credential: "{{ result_auth }}"
     workflow_name: "ansible_sample_workflow_SY1"
 
 - name: Check the status of a workflow
   ibm.ibm_zosmf.zmf_workflow:
     state: "check"
-    zmf_host: "sample.ibm.com"
+    zmf_credential: "{{ result_auth }}"
     workflow_name: "ansible_sample_workflow_SY1"
 """
 
